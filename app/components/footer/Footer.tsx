a02c41eb8706b41c9c2297258696fd0801f8e031
Arqam Tahir <muhammadarqam920@gmail.com>
2024-10-26 05:12:24 +0500

    components/header-footer: add header and footer structure

---
+++ app/components/footer/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(): JSX.Element {
+  return (
+    <footer className="text-dark p-4 bg-074b6c">
+      <p className="">© 2024 Storify. All rights reserved.</p>
+    </footer>
+  );
+}
