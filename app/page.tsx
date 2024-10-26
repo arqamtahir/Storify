4930616436b8adefd797a4a50296948941d0c290
Arqam Tahir <muhammadarqam920@gmail.com>
2024-10-27 02:09:52 +0500

    components/header-footer: add framer-motion for animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,10 @@
-import Image from "next/image";
+import TopSection from "@/app/components/homePage/TopSection"
 
 export default function Home() {
   return (
     <div className="bg-fffcd4-074b6c mx-auto grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] ">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full h-full">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full h-full">
-          {/* Left Column: Image */}
-          <div className="block justify-center items-center w-full h-full">
-            <Image
-              src="https://picsum.photos/800/400" // Update with your image path
-              alt="Description of image"
-              width={500} // Adjust width as needed
-              height={500} // Adjust height as needed
-              className="object-cover w-full h-auto rounded-full" // Ensures image is responsive
-            />
-            <p className="text-center pt-7">What is Lorem Ipsum?
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-          </div>
-
-          {/* Right Column: Text */}
-          <div className="flex flex-col justify-center items-start">
-            <p className="text-center">What is Lorem Ipsum?
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            <div className="flex gap-4 items-center w-full justify-center pt-7">
-              <button className="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
-                Filled
-              </button>
-              <button className="rounded-md bg-gradient-to-tr from-slate-800 to-slate-700 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
-                Gradient
-              </button>
-              <button className="rounded-md border border-slate-300 py-2 px-4 text-center text-sm transition-all shadow-sm hover:shadow-lg text-slate-600 hover:text-white hover:bg-slate-800 hover:border-slate-800 focus:text-white focus:bg-slate-800 focus:border-slate-800 active:border-slate-800 active:text-white active:bg-slate-800 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
-                Outlined
-              </button>
-              <button className="rounded-md border border-transparent py-2 px-4 text-center text-sm transition-all text-slate-600 hover:bg-slate-100 focus:bg-slate-100 active:bg-slate-100 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
-                Text
-              </button>
-            </div>
-          </div>
-        </div>
+        <TopSection />
       </main>
     </div>
   );
