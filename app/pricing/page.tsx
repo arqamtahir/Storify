0143a20330958428040d14c67c9d69015b717fec
Arqam Tahir <muhammadarqam920@gmail.com>
2024-11-17 00:49:15 +0500

    components/homePage-section: add pricing page content

---
+++ app/pricing/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import Pricing from "../components/pricing/Pricing";
+
+export default function Home() {
+  return (
+    <div className="mx-auto grid grid-rows-[0px_1fr_20px] items-center justify-items-center min-h-screen pb-20 gap-16 font-[family-name:var(--font-geist-sans)] bg-base">
+      <main className="flex flex-col gap-0 row-start-2 items-center sm:items-start w-full h-full">
+        <Pricing/>
+      </main>
+    </div>
+  );
+}
