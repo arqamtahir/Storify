6ee93c8b45590d96a32ef04e94ff9db20c220e1e
Arqam Tahir <muhammadarqam920@gmail.com>
2024-11-27 23:51:28 +0500

    components/homePage-section: add login component

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,14 @@
 "use client"
+import TopSection from "@/app/components/login/TopSection"
 
-import TopSection from "@/app/components/contact/TopSection"
-// import ConnectThree from "@/app/components/homePage/animatedImage/ConnectThree"
-
-export default function Contact() {
+export default function Login() {
   return (
     <div className="mx-auto grid grid-rows-[0px_1fr_20px] items-center justify-items-center min-h-screen pb-20 gap-16 font-[family-name:var(--font-geist-sans)] bg-base">
       <main className="flex flex-col gap-0 row-start-2 items-center sm:items-start w-full h-full">
         <TopSection/>
       </main>
     </div>
-  );
+  )
 }
+
+
